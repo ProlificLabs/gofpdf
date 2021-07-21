0da0d0bc5e7fe637a37f01dc210e51e3eec3cfbb
Hubert Krauze <krhubert@gmail.com>
2021-07-21 15:37:29 +0200

    Update module name

--- contrib/gofpdi/gofpdi_test.go
+++ contrib/gofpdi/gofpdi_test.go
@@ -2,8 +2,8 @@ package gofpdi
 
 import (
 	"bytes"
-	"github.com/jung-kurt/gofpdf"
-	"github.com/jung-kurt/gofpdf/internal/example"
+	"github.com/krhubert/gofpdf"
+	"github.com/krhubert/gofpdf/internal/example"
 	"io"
 	"sync"
 	"testing"
